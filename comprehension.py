f5b3f85c0dac34c4ba63bf32877e894d8b35fc8d
Kurucz Péter <k.fantoser@gmail.com>
2017-07-08 21:16:38 +0200

    Commented every line in code

--- comprehension.py
+++ comprehension.py
@@ -1,33 +1,36 @@
-import random
+# A number guess game where the player can add their name and have to guess the number from 6 guess 
+# to which the program tell the guessed number is too low or high.
 
-guessesTaken = 0
+import random  # Import the random library
 
-print('Hello! What is your name?')
-myName = input()
+guessesTaken = 0  # Initialize and assign 0 to gessesTaken variable 
 
-number = random.randint(1, 20)
-print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
+print('Hello! What is your name?')  # Print out the message "Hello! What is your name?"
+myName = input()  # initialize and assign the user's input to myName variable
 
-while guessesTaken < 6:
-    print('Take a guess.')
-    guess = input()
-    guess = int(guess)
+number = random.randint(1, 20)  # The program generate a random number between 1 and 20
+print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')  # Print out a message and insert the myName variable in it
 
-    guessesTaken += 1
+while guessesTaken < 6:  # Loop trough the rest of the program until guessesTaken variable below 6
+    print('Take a guess.')  # Print the message "Take a guess"
+    guess = input()  # Initalize and assign the user's input to guess variable
+    guess = int(guess)  # convert the convert variable into integer
 
-    if guess < number:
-        print('Your guess is too low.')
+    guessesTaken += 1  # Add 1 to the guessesTaken variable
 
-    if guess > number:
-        print('Your guess is too high.')
+    if guess < number:  # Until the variable guess value is smaller than the number variable value
+        print('Your guess is too low.') # Print out "Your guess is too low"
 
-    if guess == number:
-        break
+    if guess > number: # Until the variable guess value is bigger than the number variable value
+        print('Your guess is too high.') # Print out "Your guess is too high"
 
-if guess == number:
-    guessesTaken = str(guessesTaken)
-    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
+    if guess == number: # If the guess variable value is equal to the number variable value
+        break # Break from loop
 
-if guess != number:
-    number = str(number)
-    print('Nope. The number I was thinking of was ' + number)
+if guess == number: # If the guess variable value is equal to the number variable value
+    guessesTaken = str(guessesTaken) # Convert the variable guessesTaken to a string variable
+    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!') # Print out a sentence with the myName and guessesTaken variable values insert in
+
+if guess != number: # if variable guess not equal to variable number
+    number = str(number) # Convert variable number to a string variable
+    print('Nope. The number I was thinking of was ' + number) # Print out a sentce with the number variable inserted in
